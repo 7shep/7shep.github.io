f09c1a056cce96cecf2c082b4512e40e10327958
Alex <sheppenguin@gmail.com>
2023-04-13 14:58:17 -0400

    day juan

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 // Welcome to Shep's Backend Project. Super Organzied. Everything Commented. Super Readable.
 
 //!-----IMPORTS-----!
-var data = require('mysql');
+const initSql = require('sql.js');
+
+const SQL = await initSql({
+    locateFile: file => `https://sql.js.org/dist${file}`
+});
+
+const db = 'information.db'
 
 // !-----Creating the connection-----!
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'shep',
-    password: 'secret',
-    database: 'customer_info'
-});
-
-con.connect(function(err) { 
-    if(err) {
-        console.error('Error: ' + err.stack);
-        return;
-    }
-
-    console.log('connected as ID: ' + connection.threadId);
-});
+const data = new SQL.Database(db);
