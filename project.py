496659938359cf796b5d0b346a10d1c52ad65d45
Alex <sheppenguin@gmail.com>
2023-04-12 23:50:33 -0400

    sql

--- project.py
+++ project.py
@@ -1,5 +1,9 @@
 # Welcome to Shep's Backend Project. Super Organzied. Everything Commented. Super Readable.
 
 #!-----IMPORTS-----!
-import mysql
+import sqlite3
 
+#!-----Basic SQL Definition-----!
+# con = connection, cur = cursor
+con = sqlite3.connect("information.db")
+cur = con.cursor()
