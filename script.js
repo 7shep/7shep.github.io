30e133750ed5ce280889e890b9a84e4c018afef2
Alex <sheppenguin@gmail.com>
2023-04-14 14:40:37 -0400

    JAVASCRIPT! YAY!!!

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Welcome to Shep's Backend Project. Super Organzied. Everything Commented. Super Readable.
 
 //!-----IMPORTS-----!
-const initSql = require('sql.js');
+const sql = require('mysql2');
 
-// Found on DOCS, I wouldnt have know what is wrong
-const SQL = initSql({
-    locateFile: file => `https://sql.js.org/dist${file}`
+//!-----Making the connection to the DB-----!
+const con = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'test'
+  });
+
+//!-----Collecting the User Data to be stored in the future-----!
+const userInfo = document.getElementById('user-info');
+
+emailField.addEventListener("click", (e) =>{
+    //Prevents the page from autoreloading
+    e.preventDefault();
+    const email = userInfo.email.value;
+    const name = userInfo.name.value;
+    const phoneNumber = userInfo.phone.value;
+    const ccNum = userInfo.ccnum.value;
+    const cvv = userInfo.cvv.value;
+    const expireDate = userInfo.exp.value;
+
 });
 
-const db = 'information.db'
-
-// !-----Creating the connection-----!
-const data = new SQL.Database(db);
