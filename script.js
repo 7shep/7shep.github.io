0747cb3a5d9546a950f4b367b696ddf01bd999ac
Alex <sheppenguin@gmail.com>
2023-04-18 14:38:22 -0400

    day 2-4

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 // Welcome to Shep's Backend Project. Super Organzied. Everything Commented. Super Readable.
 
-//!-----IMPORTS-----!
+//!-----Imports-----!
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 
-//!-----Collecting the User Data to be stored in the future-----!
-const userInfo = document.getElementById('user-info');
-const purchaseButton = document.getElementById('submit');
+//!-----Constants-----!
+const data = express();
 
-purchaseButton.addEventListener("click", (e) =>{
-    //Prevents the page from autoreloading
-    e.preventDefault();
-    const email = userInfo.email.value;
-    const name = userInfo.name.value;
-    const phoneNumber = userInfo.phone.value;
-    const ccNum = userInfo.ccnum.value;
-    const cvv = userInfo.cvv.value;
-    const expireDate = userInfo.exp.value;
-
-
-    console.log(email);
+const userInfo = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: Number,
+    ccnum: Number,
+    cvv: Number,
+    exp: String
 });
 
+const User = mongoose.model('User', userInfo);
+
+//Tells express to use bodyparser to parse the url
+data.use(bodyParser.urlencoded({extended: false}));
+
+//Gets called when the form is submitted on the frontend
+//Res = Response, Req = request
+data.post('/submit-info', (req, res) => {
+    const user = new User({
+        name: req.body['name'],
+        email: req.body['email'],
+        phone: req.body['phone'],
+        ccnum: req.body['ccnum'],
+        cvv: req.body['cvv'],
+        exp: req.body['exp']
+    });
+
+    user.save()
+        .then(() => {
+            res.send('Form submitted successfully');
+        })
+        //Catches an error if one occurs
+        .catch((err) => {
+            res.status(400).send('Error submitting form');
+        });
+});
+
+//Listens to port 5500, where my webserver is hosted
+data.listen(5500, (email) =>{
+    console.log("Server started on port 5500");
+    console.log(email);
+})
