0251ae312133b8af31589e6767f4872fea760cda
Alex <sheppenguin@gmail.com>
2023-04-24 14:25:46 -0400

    yesh

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 // Welcome to Shep's Backend Project. Super Organzied. Everything Commented. Super Readable.
 
 //!-----Imports-----!
-const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 //!-----Constants-----!
 const data = express();
-
-const userInfo = new mongoose.Schema({
-    name: String,
-    email: String,
-    phone: Number,
-    ccnum: Number,
-    cvv: Number,
-    exp: String
+const port = process.env.PORT || 5500;
+mongoose.connect('mongodb://localhost/myapp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const User = mongoose.model('User', userInfo);
-
-//Tells express to use bodyparser to parse the url
-data.use(bodyParser.urlencoded({extended: false}));
-
-//Gets called when the form is submitted on the frontend
-//Res = Response, Req = request
-data.post('/submit-info', (req, res) => {
-    const user = new User({
-        name: req.body['name'],
-        email: req.body['email'],
-        phone: req.body['phone'],
-        ccnum: req.body['ccnum'],
-        cvv: req.body['cvv'],
-        exp: req.body['exp']
-    });
-
-    user.save()
-        .then(() => {
-            res.send('Form submitted successfully');
-        })
-        //Catches an error if one occurs
-        .catch((err) => {
-            res.status(400).send('Error submitting form');
-        });
+const formSchema = new mongoose.Schema({
+  phone: String,
+  email: String,
+  ccnum: String,
+  cvv: String,
+  exp: String,
 });
 
-//Listens to port 5500, where my webserver is hosted
-data.listen(5500, (name) =>{
-    console.log("Server started on port 5500");
-    console.log(name);
+const Form = mongoose.model('Form', formSchema);
+
+data.use(bodyParser.json());
+
+data.post('/submit-form', (req, res) => {
+  const form = new Form(req.body);
+
+  form.save((err, form) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving form data');
+    } else {
+      res.send('Form data saved successfully');
+    }
+  });
+});
+
+data.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+data.connect((err, client) =>{
+    if (err) {
+        console.error(err);
+        return;
+    }
 })
