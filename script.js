b9473cb263227daf5ae769e3c540538179ef4ae2
Alex <sheppenguin@gmail.com>
2023-04-27 14:59:39 -0400

    IT GOT EXTENDED!!!

--- script.js
+++ script.js
@@ -2,48 +2,46 @@
 
 //!-----Imports-----!
 const express = require('express');
-//const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
+const bodyParser = require('body-parser');
+//const path = require('path');
 
 var url = "mongodb:127.0.0.1:5500"
 
 const data = express();
-//data.use(bodyParser.urlencoded({extended: true}));
+data.use(bodyParser.urlencoded({extended: true}));
 
 var server = data.listen(5550, () => {
   var host = server.address().address
   var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, port);
 })
 
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    phone: Number,
-    ccnum: Number,
-    cvv: Number,
-    exp: String
-  });
- 
-//mongoose.connect('127.0.0.1:5500', {useNewUrlParser: true, useUnifiedTopology: true});
+// MONGODB!!
 
-//const info = mongoose.model('Information: ', userSchema)
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:5550/";
 
-data.get('/test', (req, res) => {
-  console.log('test');
-})
-
-data.post('/submit-form', (req, res, email) => {
+//When Submit is Clicked!
+data.post('/submit-form', (req, res, MongoClient) => {
   console.log('here');
   //res.sendFile(path.join(__dirname, 'home.html'));
-    let emailInput = document.getElementById('email');
-    let nameInput = document.getElementById('name');
-    let ccInput = document.getElementById('ccnum');
-    let cvvInput = document.getElementById('cvv');
-    let expInput = document.getElementById('exp');
-    let phoneInput = document.getElementById('phone');
-    console.log(emailInput, nameInput, ccInput, cvvInput, expInput, phoneInput);
+  var emailInput = req.body.email;
+  var nameInput = req.body.name;
+  var ccInput = req.body.ccnum;
+  var expDateInput = req.body.expdate;
+  var cvvInput = req.body.cvv;
+  var phoneInput = req.body.phone;
+  
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var db = db.db("information");
+    var information = [emailInput, nameInput, ccInput, expDateInput, cvvInput, phoneInput];
+    db.collection("info").insertMany(information, (err, res) => {
+      if (err) throw err;
+      console.log("Inserted " + res.insertedCount + "documents");
+      db.close();
+    });
+  });
 
 });
