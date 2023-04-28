386d5573230a1759f5e2b29de43584a443d8c1c0
Alex <sheppenguin@gmail.com>
2023-04-28 14:40:46 -0400

    wdadadadadadad

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 //!-----Imports-----!
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 //const path = require('path');
 
-var url = "mongodb:127.0.0.1:5500"
 
 const data = express();
 data.use(bodyParser.urlencoded({extended: true}));
@@ -17,13 +17,21 @@ var server = data.listen(5550, () => {
    console.log("Example app listening at http://%s:%s", host, port);
 })
 
-// MONGODB!!
+// MYSQL!
+var connection = mysql.createConnection({
+  host: 'localhost:5500',
+  user: 'root',
+  password: '',
+  database: 'information'
+})
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Database Connected!");
+})
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:5550/";
-
+module.exports = connection
 //When Submit is Clicked!
-data.post('/submit-form', (req, res, MongoClient) => {
+data.post('/submit-form', (req, res) => {
   console.log('here');
   //res.sendFile(path.join(__dirname, 'home.html'));
   var emailInput = req.body.email;
@@ -33,15 +41,16 @@ data.post('/submit-form', (req, res, MongoClient) => {
   var cvvInput = req.body.cvv;
   var phoneInput = req.body.phone;
   
-  MongoClient.connect(url, function(err, db) {
+  console.log(emailInput);
+  connection.connect((err) => {
     if (err) throw err;
-    var db = db.db("information");
-    var information = [emailInput, nameInput, ccInput, expDateInput, cvvInput, phoneInput];
-    db.collection("info").insertMany(information, (err, res) => {
-      if (err) throw err;
-      console.log("Inserted " + res.insertedCount + "documents");
-      db.close();
-    });
+    console.log("Database Connected!")
+  })
+  var sql = `INSERT INTO contacts (emailInput, nameInput, ccInput, expDateInput, cvvInput, phoneInput) VALUES ("${nameInput}", "${phoneInput}", "${expDateInput}", "${cvvInput}" "${emailInput}", "${ccInput}",   NOW())`;
+  db.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log('record inserted');
+    req.flash('success', 'Data added successfully!');
+    res.redirect('/');
   });
-
 });
