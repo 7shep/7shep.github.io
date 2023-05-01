1f92b7944f4fab1f5511fbd3bb06a9dd696d38a7
Alex <sheppenguin@gmail.com>
2023-05-01 14:59:30 -0400

    yurrski

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 //!-----Imports-----!
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
 //const path = require('path');
 
 
@@ -18,39 +17,47 @@ var server = data.listen(5550, () => {
 })
 
 // MYSQL!
-var connection = mysql.createConnection({
-  host: 'localhost:5500',
-  user: 'root',
-  password: '',
-  database: 'information'
-})
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Database Connected!");
-})
+var mysql = require('mysql');
 
-module.exports = connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "shep",
+  password: "password",
+  database: "localhost/info"
+});
+
+con.connect(() => {
+  console.log("Connected!")
+});
+
+con.query(`
+  CREATE TABLE form_data (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    ccnum VARCHAR(19) NOT NULL,
+    cvv VARCHAR(4) NOT NULL,
+    expdate DATE NOT NULL,
+    PRIMARY KEY (id)
+  );`);
 //When Submit is Clicked!
-data.post('/submit-form', (req, res) => {
+data.post('/submit-form', async (req, res) => {
   console.log('here');
   //res.sendFile(path.join(__dirname, 'home.html'));
-  var emailInput = req.body.email;
-  var nameInput = req.body.name;
-  var ccInput = req.body.ccnum;
-  var expDateInput = req.body.expdate;
-  var cvvInput = req.body.cvv;
-  var phoneInput = req.body.phone;
+  var email = req.body.email;
+  var name = req.body.name;
+  var cc = req.body.ccnum;
+  var expdate = req.body.expdate;
+  var cvv = req.body.cvv;
+  var phone = req.body.phone;
   
-  console.log(emailInput);
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log("Database Connected!")
-  })
-  var sql = `INSERT INTO contacts (emailInput, nameInput, ccInput, expDateInput, cvvInput, phoneInput) VALUES ("${nameInput}", "${phoneInput}", "${expDateInput}", "${cvvInput}" "${emailInput}", "${ccInput}",   NOW())`;
-  db.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log('record inserted');
-    req.flash('success', 'Data added successfully!');
-    res.redirect('/');
-  });
+  try {
+    let sql = `INSERT INTO info (name, email, phone, ccnum, cvv, expdate) VALUES (?, ?, ?, ?, ?, ?)`;
+    console.log('Form data inserted into table.');
+    res.send('Form data submitted successfully.');
+  } catch (err) {
+    console.error('Error inserting data into database: ' + err.stack);
+    res.status(500).send('Database error: Please try again later.');
+  }
 });
