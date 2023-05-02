843f7b68e9a4311b71138afced1f31238d8e0ff8
Alex <sheppenguin@gmail.com>
2023-05-02 14:57:22 -0400

    firebase!

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 //!-----Imports-----!
 const express = require('express');
 const bodyParser = require('body-parser');
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 //const path = require('path');
 
 
@@ -16,48 +18,55 @@ var server = data.listen(5550, () => {
    console.log("Example app listening at http://%s:%s", host, port);
 })
 
-// MYSQL!
-var mysql = require('mysql');
+//FIREBASE!
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "shep",
-  password: "password",
-  database: "localhost/info"
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAcmnXAW1HfIBaUMIij4AAgOn-zlFik58E",
+  authDomain: "sheps-tej4m.firebaseapp.com",
+  projectId: "sheps-tej4m",
+  storageBucket: "sheps-tej4m.appspot.com",
+  messagingSenderId: "978374437289",
+  appId: "1:978374437289:web:1cc4ccee240dcc5fe3fcb8",
+  measurementId: "G-XY154L261Q"
+};
 
-con.connect(() => {
-  console.log("Connected!")
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-con.query(`
-  CREATE TABLE form_data (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(15) NOT NULL,
-    ccnum VARCHAR(19) NOT NULL,
-    cvv VARCHAR(4) NOT NULL,
-    expdate DATE NOT NULL,
-    PRIMARY KEY (id)
-  );`);
+import { getDatabase, ref, set } from "https://sheps-tej4m-default-rtdb.firebaseio.com/";
+
+
 //When Submit is Clicked!
 data.post('/submit-form', async (req, res) => {
   console.log('here');
   //res.sendFile(path.join(__dirname, 'home.html'));
-  var email = req.body.email;
-  var name = req.body.name;
-  var cc = req.body.ccnum;
-  var expdate = req.body.expdate;
-  var cvv = req.body.cvv;
-  var phone = req.body.phone;
-  
-  try {
-    let sql = `INSERT INTO info (name, email, phone, ccnum, cvv, expdate) VALUES (?, ?, ?, ?, ?, ?)`;
-    console.log('Form data inserted into table.');
-    res.send('Form data submitted successfully.');
-  } catch (err) {
-    console.error('Error inserting data into database: ' + err.stack);
-    res.status(500).send('Database error: Please try again later.');
-  }
+  let email = req.body.email;
+  let name = req.body.name;
+  let cc = req.body.ccnum;
+  let expdate = req.body.expdate;
+  let cvv = req.body.cvv;
+  let phone = req.body.phone;
+
+  sendInformation(email, name, cc, expdate, cvv, phone);
 });
+
+function sendMessage(email, name, cc, expdate, cvv, phone) {
+  const database = getDatabase();
+
+  //Math is for a random UID between 1 and 10000
+  set(ref(database, 'users/' + Math.floor(Math.random() * 10000)), {
+    name: name,
+    email: email,
+    creditcard: cc,
+    expiredate: expdate,
+    cvv: cvv,
+    phonenumber: phone
+  }).then(() => {
+
+  })
+}
+
+
